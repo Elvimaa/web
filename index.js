fb6e858fd3fe7d2216c541e96b7d1b3a4b5df941
Elvimaa <151628210+Elvimaa@users.noreply.github.com>
2024-12-02 13:46:53 +0500

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<script src="jquery.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#repPass').change(function() {
+            var pass = $("#pass").val();
+            var pass_rep = $("#repPass").val();
+
+            if (pass != pass_rep) {
+                $("#repPass").css('border', 'red 1px solid');
+                $('#errorBlock').html('Пароли не совпадают');
+            }
+        });
+    });
+</script>
+<div id="errorBlock"></div>
