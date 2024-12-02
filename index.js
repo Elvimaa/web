01a7abdf2acfbd01701febbfb22239fcb5c457f3
Elvimaa <151628210+Elvimaa@users.noreply.github.com>
2024-12-02 14:14:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-<script src="jquery.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#repPass').change(function() {
-            var pass = $("#pass").val();
-            var pass_rep = $("#repPass").val();
+$(document).ready(function() {
+        $('#confirm-password').change(function() {
+            var pass = $("#reg-password").val();
+            var pass_rep = $("#confirm-password").val();
 
-            if (pass != pass_rep) {
-                $("#repPass").css('border', 'red 1px solid');
+            if (reg-password != confirm-password) {
+                $("#confirm-password").css('border', 'red 1px solid');
                 $('#errorBlock').html('Пароли не совпадают');
             }
         });
     });
-</script>
-<div id="errorBlock"></div>
